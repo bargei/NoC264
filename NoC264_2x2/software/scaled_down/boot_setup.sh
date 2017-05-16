e82ab974e0c1d07391be8950024c1f3ac6a8c290
bargei <ianjamesbarge@gmail.com>
2017-05-15 22:28:23 -0500

    A couple more fixes after readme testing

--- NoC264_2x2/software/scaled_down/boot_setup.sh
+++ NoC264_2x2/software/scaled_down/boot_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp boot_script.sh /etc/init.d/noc_264_boot_script.sh
-update-rc.d /etc/init.d/noc_264_boot_script.sh
+update-rc.d noc_264_boot_script.sh defaults
 mv programfpga programfpga_backup
 
